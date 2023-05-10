1d0590c1f39fbd756b218c6f93e5bc2b03458350
zhouzhou.wang <zhouzhou.wang@thoughtworks.com>
2023-05-10 19:47:00 +0800

    [part5]refactor: add dependency for test application

--- TDD/build.gradle.kts
+++ TDD/build.gradle.kts
@@ -10,13 +10,17 @@ repositories {
 }
 
 dependencies {
+    implementation("org.hibernate:hibernate-core:5.4.24.Final")
+    implementation("jakarta.persistence:jakarta.persistence-api:2.2.2")
+    implementation("org.hsqldb:hsqldb:2.7.1")
+
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_20
-    targetCompatibility = JavaVersion.VERSION_20
+    sourceCompatibility = JavaVersion.VERSION_16
+    targetCompatibility = JavaVersion.VERSION_16
 }
 
 tasks.test {
