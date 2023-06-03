1244c782a09291258c6a8a08955398060b7e98a5
zhouzhou.wang <zhouzhou.wang@thoughtworks.com>
2023-06-03 20:48:41 +0800

    [train] feature: can build two city graph model

--- TDD/src/main/java/com/thoughtworks/train/City.java
+++ TDD/src/main/java/com/thoughtworks/train/City.java
@@ -21,7 +21,23 @@ public class City {
     }
 
     public static City generate(List<String> input) {
-        return null;
+        String params = input.get(0);
+        String startCityName = params.substring(0, 1);
+        String destinationCityName = params.substring(1, 2);
+        Integer distance = Integer.parseInt(params.substring(2));
+        City startCity = City.builder()
+                .name(startCityName)
+                .build();
+        City destinationCity = City.builder()
+                .name(destinationCityName)
+                .build();
+        List<ArriveCity> arriveCities = startCity.getCanArriveCities();
+        arriveCities.add(ArriveCity.builder()
+                .city(destinationCity)
+                .distance(distance)
+                .build());
+
+        return startCity;
     }
 
 }
