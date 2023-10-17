d04c4793789ca3f8cb7711cdbb46bf038b4210d0
zhouzhou.wang <zhouzhou.wang@thoughtworks.com>
2023-10-17 19:32:03 +0800

    [Backend] chore: rename table field

--- backend-study/Part01/src/main/resources/db/migration/V1.0.0__init_database_table.sql
+++ backend-study/Part01/src/main/resources/db/migration/V1.0.0__init_database_table.sql
@@ -16,8 +16,8 @@ create table t_order
     status           varchar(32) default 'CREATED' not null,
     create_time      datetime                      not null,
     create_by        int                           not null,
-    receivingAddress varchar(255)                  not null,
-    receivingNumber  varchar(11)                   not null
+    receiving_address varchar(255)                  not null,
+    receiving_number  varchar(11)                   not null
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
 
