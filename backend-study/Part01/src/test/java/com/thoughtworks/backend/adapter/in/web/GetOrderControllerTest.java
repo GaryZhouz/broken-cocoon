e08ac9a4025c5701362450eb2ce5087781e2da53
zhouzhou.wang <zhouzhou.wang@thoughtworks.com>
2023-10-18 15:31:46 +0800

    [Backend] test: add test for order api

---
+++ backend-study/Part01/src/test/java/com/thoughtworks/backend/adapter/in/web/GetOrderControllerTest.java
@@ -0,0 +1,77 @@
+package com.thoughtworks.backend.adapter.in.web;
+
+import com.thoughtworks.backend.JUnitWebAppTest;
+import com.thoughtworks.backend.application.domain.model.order.Order;
+import com.thoughtworks.backend.application.domain.model.order.OrderProduct;
+import com.thoughtworks.backend.application.port.in.GetOrderUseCase;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@JUnitWebAppTest
+public class GetOrderControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private GetOrderUseCase getOrderUseCase;
+
+    @Test
+    public void should_return_orders() throws Exception {
+        List<OrderProduct> orderProducts = List.of(
+                new OrderProduct("1", "iPhone 12", 1, "5999"),
+                new OrderProduct("1", "Mac Book Pro 2019", 1, "10999")
+        );
+        Order order = new Order(1L, orderProducts, "16998", Order.OrderStatus.CREATED, LocalDateTime.now(), "customer-01", "地球", "15566666666");
+
+        when(getOrderUseCase.getOrders("1")).thenReturn(List.of(order));
+
+        mockMvc.perform(get("/1/orders")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data[*].id", contains(1)))
+                .andExpect(jsonPath("$.data[*].products[*].name", contains("iPhone 12", "Mac Book Pro 2019")))
+                .andExpect(jsonPath("$.data[*].products[*].price", contains("5999", "10999")))
+                .andExpect(jsonPath("$.data[*].status", contains("CREATED")))
+                .andExpect(jsonPath("$.data[*].totalPrice", contains("16998")));
+
+        verify(getOrderUseCase).getOrders("1");
+    }
+
+    @Test
+    public void should_return_order_when_given_order_id() throws Exception {
+        List<OrderProduct> orderProducts = List.of(
+                new OrderProduct("1", "iPhone 12", 1, "5999"),
+                new OrderProduct("1", "Mac Book Pro 2019", 1, "10999")
+        );
+        Order order = new Order(1L, orderProducts, "16998", Order.OrderStatus.CREATED, LocalDateTime.now(), "customer-01", "地球", "15566666666");
+
+        when(getOrderUseCase.getOrder(1L)).thenReturn(order);
+
+        mockMvc.perform(get("/1/order")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data.id", is(1)))
+                .andExpect(jsonPath("$.data.products[*].name", contains("iPhone 12", "Mac Book Pro 2019")))
+                .andExpect(jsonPath("$.data.products[*].price", contains("5999", "10999")))
+                .andExpect(jsonPath("$.data.status", is("CREATED")))
+                .andExpect(jsonPath("$.data.totalPrice", is("16998")));
+
+        verify(getOrderUseCase).getOrder(1L);
+    }
+
+}
--- backend-study/Part01/src/test/java/com/thoughtworks/backend/adapter/out/persistence/OrderPersistenceAdapterTest.java
+++ backend-study/Part01/src/test/java/com/thoughtworks/backend/adapter/out/persistence/OrderPersistenceAdapterTest.java
@@ -1,10 +1,13 @@
 package com.thoughtworks.backend.adapter.out.persistence;
 
+import com.thoughtworks.backend.adapter.out.persistence.entity.OrderEntity;
+import com.thoughtworks.backend.adapter.out.persistence.entity.OrderProductEntity;
 import com.thoughtworks.backend.adapter.out.persistence.mapper.OrderMapper;
 import com.thoughtworks.backend.adapter.out.persistence.mapper.OrderProductMapper;
 import com.thoughtworks.backend.application.domain.model.order.Order;
 import com.thoughtworks.backend.application.domain.model.order.OrderProduct;
 import com.thoughtworks.backend.application.port.in.CreateOrderCommand;
+import com.thoughtworks.backend.common.exception.BusinessException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -15,12 +18,12 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-
 public class OrderPersistenceAdapterTest {
 
     private OrderPersistenceAdapter orderPersistenceAdapter;
@@ -48,16 +51,64 @@ public class OrderPersistenceAdapterTest {
         CreateOrderCommand createOrderCommand = new CreateOrderCommand(
                 createOrderProducts, "customer-01", "地球", "15566666666"
         );
-        Order order = new Order(1L, orderProducts, "16998", Order.OrderStatus.CREATED, LocalDateTime.now(), "customer-01", "地球", "15566666666");
+        Order order = new Order(1L, orderProducts, "16998.0", Order.OrderStatus.CREATED, LocalDateTime.now(), "customer-01", "地球", "15566666666");
 
-        when(orderPersistenceAdapter.createOrder(createOrderCommand)).thenReturn(order);
+        when(orderMapper.insert(any())).thenReturn(1);
+        when(orderProductMapper.insertBatchSomeColumn(any())).thenReturn(1);
 
         Order result = orderPersistenceAdapter.createOrder(createOrderCommand);
 
-        assertEquals(result, order);
+        assertEquals(result.getCreateBy(), order.getCreateBy());
+        assertEquals(result.getTotalPrice(), order.getTotalPrice());
+        assertEquals(result.getStatus(), order.getStatus());
 
         verify(orderMapper).insert(any());
         verify(orderProductMapper).insertBatchSomeColumn(any());
+    }
+
+    @Test
+    public void should_return_orders() {
+        List<OrderProductEntity> orderProducts = List.of(
+                new OrderProductEntity(1L, 1L, "1", "iPhone 12", "5999", 1),
+                new OrderProductEntity(1L, 1L, "1", "Mac Book Pro 2019", "10999", 1)
+        );
+        OrderEntity orderEntity = new OrderEntity(1L, "16998", Order.OrderStatus.CREATED, LocalDateTime.now(), "customer-01", "地球", "15566666666");
+
+        when(orderMapper.selectList(any())).thenReturn(List.of(orderEntity));
+        when(orderProductMapper.selectList(any())).thenReturn(orderProducts);
+
+        List<Order> result = orderPersistenceAdapter.getOrders("1");
+
+        assertEquals(result.get(0).getId(), orderEntity.getId());
+        assertEquals(result.get(0).getTotalPrice(), orderEntity.getTotalPrice());
+        assertEquals(result.get(0).getStatus(), orderEntity.getStatus());
+
+        verify(orderMapper).selectList(any());
+        verify(orderProductMapper).selectList(any());
+    }
+
+    @Test
+    public void should_return_order_by_order_id() {
+        OrderEntity orderEntity = new OrderEntity(1L, "16998", Order.OrderStatus.CREATED, LocalDateTime.now(), "customer-01", "地球", "15566666666");
+
+        when(orderMapper.selectById(any())).thenReturn(orderEntity);
+
+        Order result = orderPersistenceAdapter.getOrder(1L);
+
+        assertEquals(result.getId(), orderEntity.getId());
+        assertEquals(result.getTotalPrice(), orderEntity.getTotalPrice());
+        assertEquals(result.getStatus(), orderEntity.getStatus());
+
+        verify(orderMapper).selectById(any());
+    }
+
+    @Test
+    public void should_throw_exception_when_order_id_not_exist() {
+        when(orderMapper.selectById(any())).thenReturn(null);
+
+        assertThrows(BusinessException.class, () -> orderPersistenceAdapter.getOrder(1L));
+
+        verify(orderMapper).selectById(any());
     }
 
 }
